ec7c3f37d28456e2cf095d91d02ab0288190f78b
Charlie Lloyd <sccjl@leeds.ac.uk>
2018-01-11 17:35:55 +0000

    Time series analysis

--- code/timeSeriesAnalysis.py
+++ code/timeSeriesAnalysis.py
@@ -14,10 +14,10 @@ import matplotlib.pyplot as plt
 from movingAverageFilterReynoldsStresses import movingWeightedAverage as mwa
 #
 ###########################################################################################
-fil = pd.read_pickle('../data/processedData/smoothPlate/4Hz/x400/171211_4Hz_x400/timeSeries/fil/4Hz_x400.000049.pkl')
-raw = pd.read_pickle('../data/processedData/smoothPlate/4Hz/x400/171211_4Hz_x400/timeSeries/raw/4Hz_x400.000049.pkl')
-
-filAv = pd.read_pickle('../data/processedData/smoothPlate/4Hz/x400/171211_4Hz_x400/171211_4Hz_x400.pkl')
+fil = pd.read_pickle('../data/processedData/smoothPlate/4Hz/x400/171211_4Hz_x400/timeSeries/min/min_4Hz_x400.000049.pkl')
+raw = pd.read_pickle('../data/processedData/smoothPlate/4Hz/x400/171211_4Hz_x400/timeSeries/raw/raw_4Hz_x400.000049.pkl')
+#print(fil,raw)
+#filAv = pd.read_pickle('../data/processedData/smoothPlate/4Hz/x400/171211_4Hz_x400/171211_4Hz_x400.pkl')
 
 #	create time series for RS
 UxMean = mwa(200,fil.Ux.as_matrix(),fil.resTime.as_matrix())
@@ -30,6 +30,7 @@ plt.subplot(2,3,1)
 plt.plot(fil.timeStamp,fil.Ux)
 plt.subplot(2,3,2)
 plt.plot(fil.timeStamp,fil.Uy)
+plt.plot(fil.timeStamp,UyMean)
 plt.subplot(2,3,3)
 plt.plot(fil.timeStamp,ruu)
 plt.subplot(2,3,4)
